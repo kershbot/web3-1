cfd4299f6e63891b18775f6d59de2cef050cfe81
sheva29 <sancm644@newschool.edu>
2013-03-05 17:53:20 -0500

    Update mauricio_give.php
    Grouped everything with a new key called company companies

--- week5/mauricio_give.php
+++ week5/mauricio_give.php
@@ -7,19 +7,14 @@
 
 			$info = array('General Info' => array('Mauricio Sanchez-Duque','3252 41th st. Apt 1A','Astoria,NY 11103', '3474764749','design@sanchezmauricio.com','http://www.sanchezmauricio.com'
 												 ),
-						  'Experience' => array('PARSONS THE NEW SCHOOL FOR DESIGN',
+						  'Experience' => array( 'Company' =>'PARSONS THE NEW SCHOOL FOR DESIGN',
 						  						'Teacher Assistant / Jan 2013 - Present' => array('Assist students and professor during the Interaction Core Design class',
 						  																		  'Guide students through the design process, assessing concepts and debugging prototypes', 
 						  																		  'Provide students with web and mobile technologies in their projects'
 						  																		 ),
-						  						'GLIDE-­INC',
-						  						'TIGERSPIKE',
-						  						'BUBBLEBEATS',
-						  						'QUADRANT2 INC',
-						  						'HABANA OUTPOST',
-						  						'THE TWISTTOGETHER COMPANY',
-						  						'MARTEL DESIGN',
-						  						'PARSONS THE NEW SCHOOL FOR DESIGN',
+						  						
+						  						'Companies' =>array('GLIDE-­INC','TIGERSPIKE','BUBBLEBEATS','QUADRANT2 INC','HABANA OUTPOST',
+						  											'THE TWISTTOGETHER COMPANY','MARTEL DESIGN','PARSONS THE NEW SCHOOL FOR DESIGN'),
 						  						'Freelance Designer / Jan 2009 - Present' => array('Interaction Design',
 						  																		   'Product Development',
 						  																		   'Branding',
@@ -27,7 +22,7 @@
 						  																		   'UX/UI',
 						  																		   'Design Consulting'
 						  																		 ),
-						  						'GLIDE-­INC',
+						  						'Company' => 'GLIDE-­INC',
 						  						'Junior Industrial Designer / Jan 2008 - Feb 2009' => array('Product Design in furniture and lighting',
 						  																					'Product Photography and Interior Design',
 						  																					'Develop new products from conception to production'
@@ -68,7 +63,7 @@
 
 						  
 	);
-			$char = '$\ / ; " ';
+			/*$char = '$\ / ; " ';*/
 			
 			echo json_encode($info);
 		}
